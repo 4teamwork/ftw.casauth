8c19d19026cfe9b5cac0bc3035cb04f998190c50
Lukas Graf <lukas.graf@4teamwork.ch>
2021-02-02 15:49:50 +0100

    Preserve multi-valued QS params when stripping ticket from service URL.

--- ftw/casauth/tests/test_cas.py
+++ ftw/casauth/tests/test_cas.py
@@ -1,9 +1,11 @@
-import unittest
+from ftw.casauth.cas import strip_ticket
+from ftw.casauth.cas import validate_ticket
+from ftw.casauth.tests.utils import get_data
 from ftw.casauth.tests.utils import MockRequest
 from ftw.casauth.tests.utils import MockResponse
-from ftw.casauth.tests.utils import get_data
-from ftw.casauth.cas import validate_ticket
 from mock import patch
+
+import unittest
 
 
 class TestValdidateTicket(unittest.TestCase):
@@ -45,3 +47,21 @@ class TestValdidateTicket(unittest.TestCase):
             'ST-001-abc',
             'https://cas.domain.net',
             'https://service.domain.net'))
+
+
+class TestStripTicket(unittest.TestCase):
+
+    def test_strip_ticket_drops_ticket_from_url(self):
+        url = 'http://example.org?ticket=ST-001-abc&param1=v1&param2=v2'
+        stripped = strip_ticket(url)
+        self.assertEqual('http://example.org?param1=v1&param2=v2', stripped)
+
+    def test_strip_ticket_preserves_multi_valued_params(self):
+        url = 'http://example.org?ticket=ST-001-abc&multi=v1&multi=v2'
+        stripped = strip_ticket(url)
+        self.assertEqual('http://example.org?multi=v1&multi=v2', stripped)
+
+    def test_strip_ticket_preserves_zope_style_multi_valued_params(self):
+        url = 'http://example.org?ticket=ST-001-abc&multi:list=v1&multi:list=v2'
+        stripped = strip_ticket(url)
+        self.assertEqual('http://example.org?multi%3Alist=v1&multi%3Alist=v2', stripped)
